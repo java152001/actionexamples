04adb0c043a165336af80b3f5f448cdeb665b581
Anthony Demott <java152001@gmail.com>
2026-03-16 16:36:46 -0700

    Updated action scripts and test functions

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./public/hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World Yes");
+    expect(hello.hello()).toEqual("Hello World Yeahhhhh");
   });
 });
--- public/hello.js
+++ public/hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World Yes";
+const hello = () => "Hello World Yeahhhhh";
 console.log(hello());
 exports.hello = hello;
