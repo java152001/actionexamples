a9e17b4c3d8f0d8338d71a55fbdf24473a22fe15
Anthony Demott <java152001@gmail.com>
2026-03-16 15:30:48 -0700

    Updated tests

--- hello.test.js
+++ hello.test.js
@@ -1,4 +1,4 @@
-const hello = require("./hello");
+const hello = require("./public/hello");
 describe("My hello", () => {
   test("works", () => {
     expect(hello.hello()).toEqual("Hello World Yes");
